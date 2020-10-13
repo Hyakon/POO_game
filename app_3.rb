a18a2d3437e21a7dc4db3c9c3a3e7943bc459e2e
Hyakon <azn.baoul@gmail.com>
2020-10-13 13:50:05 +0200

    part 2 done

--- app_3.rb
+++ app_3.rb
@@ -6,11 +6,29 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-g = Game.new('name')
+# noinspection SpellCheckingInspection
+puts "------------------------------------------------
+|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |
+|Le but du jeu est d'être le dernier survivant !|
+-------------------------------------------------"
+puts "What's your Player name"
+name = gets.chomp
 
-g.human_player.show_state
-g.enemies.each do |enemy|
-  enemy.show_state
+my_game = Game.new(name)
+round = 1
+while my_game.still_ongoing?
+  puts '=' * 25
+  puts "Round #{round}".center(25)
+  puts '=' * 25
+  round += 1
+
+  my_game.show_player
+
+  my_game.menu
+  choice = gets.chomp
+  my_game.menu_choice(choice)
+  my_game.enemies_attack
+
 end
 
-binding.pry
+my_game.end
